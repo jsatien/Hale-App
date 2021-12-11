9b2293029d42e7401cd6c5653c7fb76081c9a2ba
I'm_Great <therdysojeramop@gmail.com>
2021-12-11 11:09:52 +0800

    Day 2

--- HaleApp/App.js
+++ HaleApp/App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { Searchbar } from 'react-native-paper';
 
 export default function App() {
+    const [searchQuery, setSearchQuery] = React.useState('');
+    const onChangeSearch = query => setSearchQuery(query);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={{padding:15, backgroundColor: "green"}}>
+        <Searchbar
+      placeholder="Search"
+      onChangeText={onChangeSearch}
+      value={searchQuery}
+    />
+        </View>
+        <View style={styles.search}>
+          <Text>
+            Search
+          </Text>
+        </View>
+        <View style={styles.list}>
+            <Text>
+              List
+            </Text>
+          </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto"/>
+    </>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1, 
+    marginTop: StatusBar.currentHeight, 
+    backgroundColor: "orange" 
   },
+  search: {
+    flex:1, 
+    padding:15, 
+    backgroundColor: "orange"
+  },
+  list: {
+    flex:1, 
+    padding:15, 
+    backgroundColor: "blue"
+  }
 });
